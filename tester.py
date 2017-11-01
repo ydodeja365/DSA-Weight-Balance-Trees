a2d0ba5883160a92859d548444765204c0f882ce
srinskit <srinagrao2007@gmail.com>
2017-11-01 11:46:13 +0530

    Tester

--- tester.py
+++ tester.py
@@ -3,6 +3,17 @@ from WBTree import WBT
 
 def main():
     tree = WBT()
+    data = open('data')
+    for val in data:
+        tree.insert(int(val))
+    data.close()
+    tree.print_tree()
+    data = open('data')
+    for val in data:
+        if tree.search(int(val)) is None:
+            # print(val)
+            print(':(')
+    print('Done')
 
 
 if __name__ == '__main__':
