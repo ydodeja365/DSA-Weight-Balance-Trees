4e48ee0d610526635e5885ca4469a28fff3b29e6
srinskit <srinagrao2007@gmail.com>
2017-11-05 06:32:31 +0530

    insert and search for passenger class

--- MapTester.py
+++ MapTester.py
@@ -1,16 +1,24 @@
 from HashMap import *
 
 
+class Passenger(Pair):
+    def __init__(self, details):
+        Pair.__init__(self, details[-1])
+        self.details = details
+
+
 def main():
+    data = open('dataset.csv')
     H = HashMap(20)
-    my_list = [Pair('16IT141', 'Srinag'), Pair('16IT135', 'Shreyas'), Pair('16IT149', 'Yash'),
-               Pair('16IT211', 'Bharath')]
-    keys = ['16IT141', '16IT142', '16IT135', '16IT149', '16IT211']
-    for pair in my_list:
-        H.insert(pair)
+    keys = []
+    passenger = None
+    for line in data:
+        passenger = Passenger(tuple(line.split(',')[1:]))
+        keys.append(passenger.key)
+        H.insert(passenger)
     for key in keys:
         try:
-            print(H.search(key).val)
+            print(H.search(key).details[5])
         except:
             print(key + ' not found')
 
