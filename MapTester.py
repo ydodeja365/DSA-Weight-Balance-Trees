75a77807606fb483d6504dd3c9fbb72039ce9af5
srinskit <srinagrao2007@gmail.com>
2017-11-08 22:02:14 +0530

    Support new project structure

--- MapTester.py
+++ MapTester.py
@@ -1,26 +1,27 @@
-from HashMap import *
-# from ChainedMap import *
-import pandas as  pd
+import pandas as pd
+from HashMaps.HashMap import *
+from HashMaps.ChainedMap import *
+
+
 class Passenger(Pair):
     def __init__(self, details):
         Pair.__init__(self, details[-1])
         self.details = details
 
+
 def main():
-    # data = open('dataset.csv')
-    data=pd.read_csv('dataset.csv')
-     
+    data = pd.read_csv('data_sets/titanic.csv')
+
     H = HashMap(20)
     keys = []
-    passenger = None
-    for index,row in data.iterrows():
+    for index, row in data.iterrows():
         passenger = Passenger(tuple(row))
         keys.append(passenger.key)
         H.insert(passenger)
     for key in keys:
         try:
             print(str(H.search(key).details[:])[1:-1])
-        except:
+        except AttributeError:
             print(key + ' not found')
 
 
