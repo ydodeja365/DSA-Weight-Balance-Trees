2d7062b1bd4592f459b148d71949c29eaa05ef31
srinskit <srinagrao2007@gmail.com>
2017-11-10 23:08:09 +0530

    Added testers for delete

--- WBTtest.py
+++ WBTtest.py
@@ -24,7 +24,38 @@ def main():
         stop_time = time()
         print('Searched %d elements in %fs' % (count, stop_time - start_time))
     data.close()
+    data = open('data_sets/data')
+    start_time = time()
+    for val in data:
+        if tree.delete(int(val)) is None:
+            print('Delete Failed')
+            break
+    else:
+        stop_time = time()
+        print('Deleted  %d elements in %fs' % (count, stop_time - start_time))
+    data.close()
+    tree.print_tree()
+
+
+def delete_test():
+    tree = WBT()
+    store = []
+    data = open('data_sets/data')
+    for val in data:
+        store.append(int(val))
+        tree.insert(int(val))
+    data.close()
+    for i in range(len(store)):
+        if tree.delete(store[i]) is None:
+            print('Delete Failed')
+            break
+        for j in range(i + 1, len(store)):
+            if not tree.present(store[j]):
+                print('Delete Failed')
+                break
+    print(tree.root)
 
 
 if __name__ == '__main__':
+    # delete_test()
     main()
